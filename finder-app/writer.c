6034d9968de737fc079c726878168d5aa23be774
Josh Heyse <josh@heyse.us>
2024-06-06 18:01:36 -0500

    complete assignment 2

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -9,31 +9,34 @@
 int main(int argc, char **argv) {
   openlog("writer", LOG_PID, LOG_USER);
 
-  if (argc < 2) {
+  if (argc < 3) {
     printf("Usage: writer <file> <string>\n");
     syslog(LOG_ERR, "Invalid arguments");
     return 1;
   }
 
-  int fd = open(argv[0], O_WRONLY | O_TRUNC | O_CREAT);
+  char *file = argv[1];
+  char *text = argv[2];
+
+  int fd = open(file, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
   if (fd < 0) {
-    printf("Failed to open file %s", argv[0]);
-    syslog(LOG_ERR, "Failed to open file %s", argv[0]);
+    printf("Failed to open file %s", file);
+    syslog(LOG_ERR, "Failed to open file %s", file);
     return 1;
   }
 
-  syslog(LOG_DEBUG, "Writing %s to %s", argv[0], argv[1]);
+  syslog(LOG_DEBUG, "Writing %s to %s", text, file);
 
-  size_t bytes = write(fd, argv[1], strlen(argv[1]));
+  size_t bytes = write(fd, text, strlen(text));
   if (bytes < 0) {
-    printf("Failed to write to file %s", argv[0]);
-    syslog(LOG_ERR, "Failed to write to file %s", argv[0]);
+    printf("Failed to write to file %s", file);
+    syslog(LOG_ERR, "Failed to write to file %s", file);
     return 1;
   }
 
   if (close(fd) < 0) {
-    printf("Failed to close file %s", argv[0]);
-    syslog(LOG_ERR, "Failed to close file %s", argv[0]);
+    printf("Failed to close file %s", file);
+    syslog(LOG_ERR, "Failed to close file %s", file);
     return 1;
   }
 
