f36e08e9f86242709ec1604500874c79e14fc59d
Josh Heyse <josh@heyse.us>
2024-06-08 10:25:48 -0500

    use /etc/finder-app/conf as config dir if present; added make all

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,13 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFDIR=conf
+
+if [[ -d /etc/finder-app/conf ]]; then
+  CONFDIR=/etc/finder-app/conf
+fi
+
+username=$(cat $CONFDIR/username.txt)
 
 if [ $# -lt 3 ]; then
   echo "Using default value ${WRITESTR} for string to write"
@@ -30,7 +36,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat conf/assignment.txt)
+assignment=$(cat $CONFDIR/assignment.txt)
 
 if [ $assignment != 'assignment1' ]; then
   mkdir -p "$WRITEDIR"
